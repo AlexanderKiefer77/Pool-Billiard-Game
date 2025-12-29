0e85c838aa1f0f3e8ead03bb31d476002bd2351f
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-12-29 17:30:47 +0100

    delete comments from math.js implementation

--- models/controller.class.js
+++ models/controller.class.js
@@ -16,17 +16,11 @@ export class Controller {
             this.active = false;
             const factor = 0.15;
             this.ball.vel = scale(factor, this.vector);
-            // this.ball.vel.x = factor * this.vector.x; // ersetzt mit Formel in math.js
-            // this.ball.vel.y = factor * this.vector.y; // ersetzt mit Formel in math.js
         })
     }
 
     update() {
         this.vector = sub(mouse, this.ball.pos);
-        // { // ersetzt mit Formel in math.js
-        //     x: mouse.x - this.ball.pos.x,
-        //     y: mouse.y - this.ball.pos.y,
-        // }
     }
 
     draw() {
@@ -36,24 +30,17 @@ export class Controller {
         ctx.lineWidth = 10;
         ctx.lineCap = "round";
         ctx.strokeStyle = "rgba(255, 255, 255, 0.5)";
-        ctx.translate(this.ball.pos.x, this.ball.pos.y); // in Zusammenhang mit math.js hinzugefügt
+        ctx.translate(this.ball.pos.x, this.ball.pos.y);
         ctx.beginPath();
-        // ctx.moveTo(this.ball.pos.x, this.ball.pos.y); // ersetzt mit Formel in math.js
         ctx.moveTo(0, 0);
-        // ctx.lineTo(mouse.x, mouse.y); // ersetzt mit Formel in math.js
         ctx.lineTo(this.vector.x, this.vector.y);
         ctx.stroke();
         ctx.closePath();
         // thin line
         ctx.lineWidth = 1;
         ctx.beginPath();
-        // ctx.moveTo(this.ball.pos.x, this.ball.pos.y); // ersetzt mit Formel in math.js
         ctx.moveTo(0, 0);
-        // const vectorLength = Math.sqrt(this.vector.x * this.vector.x + this.vector.y * this.vector.y); // in Zusammenhang mit mit math.js entfernt
-        const targetFar = scale(canvasNorm, normalize(this.vector));  // in Zusammenhang mit math.js hinzugefügt
-        // ctx.lineTo( // ersetzt mit Formel in math.js
-        //     this.ball.pos.x + canvasNorm / vectorLength * this.vector.x,
-        //     this.ball.pos.y + canvasNorm / vectorLength * this.vector.y);
+        const targetFar = scale(canvasNorm, normalize(this.vector));
         ctx.lineTo(targetFar.x, targetFar.y);
         ctx.stroke();
         ctx.restore();

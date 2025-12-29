7c69ca90cc682ed711327eaf878c200654d539c8
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-12-29 14:42:07 +0100

    add function handleTinyVelocities

--- Ball.js
+++ Ball.js
@@ -4,8 +4,13 @@ export class Ball {
     constructor({ pos, color, vel }) {
         this.pos = pos;
         this.color = color;
-        this.vel = vel ?? { x: 0, y: 0 };
+        this.vel = vel ?? { x: 0, y: 0 }; // Geschwindigkeit
         this.size = 18;
+        this.friction = 0.99; // Reibung
+    }
+
+    get idle() { // für Ball bewegt sich nicht mehr
+        return this.vel.x == 0 && this.vel.y == 0;
     }
 
     draw() {
@@ -19,7 +24,10 @@ export class Ball {
     update() {
         this.pos.x += this.vel.x;
         this.pos.y += this.vel.y;
+        this.vel.x *= this.friction;
+        this.vel.y *= this.friction;
         this.bounceOfWalls();
+        this.handleTinyVelocities();
     }
 
     bounceOfWalls() { // wenn Ball an Bande anstösst
@@ -39,6 +47,16 @@ export class Ball {
         } else if (this.pos.y - this.size <= margin) {
             this.pos.y = this.size + margin;
             this.vel.y *= -1;
+        }
+    }
+
+    handleTinyVelocities() {
+        const treshold = 0.04; // Grenzwert für Bewegung des Ball´s. Wert nicht zu klein oder zu groß, muss entsprechend der Bewegung festgelegt werden.
+        if (Math.abs(this.vel.x) < treshold) {
+            this.vel.x = 0; // wenn Bewegung kleiner als 0.04 ist, wird die Bewegung auf 0 gesetzt.
+        }
+        if (Math.abs(this.vel.y) < treshold) {
+            this.vel.y = 0;
         }
     }
 }

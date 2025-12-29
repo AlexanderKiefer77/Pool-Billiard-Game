96a7b4a5ca74057b251466a94135febd81c3542b
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-12-29 14:22:16 +0100

    create class Ball and function for draw Ball

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+import { Ball } from "./Ball.js";
+import { clearCanvas } from "./canvas.js";
 import { drawCloth, drawWood } from "./table.js";
 
-drawCloth(); // Tischtuch zeichnen
-drawWood(); // Rand vom Tisch zeichnen
- 
+const b = new Ball({
+    pos: { x: 400, y: 300 },
+    vel: { x: 0, y: 0 },
+    color: "red",
+});
+
+function loop() {
+    clearCanvas();
+    drawCloth(); // Tischtuch zeichnen
+    drawWood(); // Rand vom Tisch zeichnen
+    b.update();
+    b.draw();
+    requestAnimationFrame(loop);
+}
+
+loop();
--- table.js
+++ table.js
@@ -2,13 +2,13 @@ import { canvas, ctx, margin } from "./canvas.js";
 
 // Tischtuch zeichnen
 export function drawCloth() {
-    ctx.fillStyle = "rgb(26, 130, 30";
+    ctx.fillStyle = "rgb(26, 130, 30)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 // Rand vom Tisch zeichnen
 export function drawWood() {
-    ctx.fillStyle = "hsl(16, 76%, 30%";
+    ctx.fillStyle = "hsl(16, 76%, 30%)";
     ctx.fillRect(0, 0, canvas.width, margin); // oben
     ctx.fillRect(0, canvas.height - margin, canvas.width, margin); // unten
     ctx.fillRect(0, 0, margin, canvas.height); // links

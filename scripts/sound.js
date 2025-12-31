7dfc7bd0fb6483f06fcc69d51517175d54f48a58
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-12-31 18:34:58 +0100

    add favicon and assets folder

--- scripts/sound.js
+++ scripts/sound.js
@@ -1,11 +1,11 @@
 export const SOUND = {
-    CUE: new Audio("./sfx/cue.mp3"),
-    COLLISION: new Audio("./sfx/collision.mp3"),
-    BUMPER: new Audio("./sfx/bumper.mp3"),
-    POCKET: new Audio("./sfx/pocket.mp3"),
-    WHIP: new Audio("./sfx/whip.mp3"),
-    WIN: new Audio("./sfx/win.mp3"),
-    LOSE: new Audio("./sfx/lose.mp3"),
+    CUE: new Audio("./assets/sfx/cue.mp3"),
+    COLLISION: new Audio("./assets/sfx/collision.mp3"),
+    BUMPER: new Audio("./assets/sfx/bumper.mp3"),
+    POCKET: new Audio("./assets/sfx/pocket.mp3"),
+    WHIP: new Audio("./assets/sfx/whip.mp3"),
+    WIN: new Audio("./assets/sfx/win.mp3"),
+    LOSE: new Audio("./assets/sfx/lose.mp3"),
 }
 
 // Beispiel für abspielen und Lautstärke
